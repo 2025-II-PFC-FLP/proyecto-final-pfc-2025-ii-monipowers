1ac8bfcb73f59964d88a49ccc16c8bb1544d527a
CristhianZuluaga12 <zuluagacristhian@correounivalle.edu.co>
2025-11-27 19:03:53 -0500

    Primera parte del proyecto completada

---
+++ app/src/main/scala/taller/RiegoOptimo.scala
@@ -0,0 +1,63 @@
+package taller
+
+object RiegoOptimo {
+  type Tablon = (Int, Int, Int)
+  type Finca = Vector[Tablon]
+  type Distancia = Vector[Vector[Int]]
+  type ProgRiego = Vector[Int]
+  type TiempoInicioRiego = Vector[Int]
+
+  val random = new scala.util.Random()
+  def fincaAlAzar(long: Int): Finca = {
+    val v = Vector.fill(long)(
+      (random.nextInt(long * 2) + 1,
+        random.nextInt(long) + 1,
+        random.nextInt(4) + 1)
+    )
+    v
+  }
+
+  def distanciaAlAzar(long: Int): Distancia = {
+    val v = Vector.fill(long, long)(random.nextInt(long * 3) + 1)
+    Vector.tabulate(long, long)((i, j) =>
+      if (i < j) v(i)(j)
+      else if (i == j) 0
+      else v(j)(i)
+    )
+  }
+
+  def tsup(f: Finca, i: Int): Int = f(i)._1
+  def treg(f: Finca, i: Int): Int = f(i)._2
+  def prio(f: Finca, i: Int): Int = f(i)._3
+
+  def tIR(f: Finca, pi: ProgRiego): TiempoInicioRiego = {
+    def tiemposEnOrden(orden: ProgRiego): Vector[Int] =
+      orden.foldLeft((Vector[Int](), 0)) { case ((acc, acum), tablon) =>
+        (acc :+ acum, acum + treg(f, tablon))
+      }._1
+    val tOrden = tiemposEnOrden(pi)
+    Vector.tabulate(f.length)(i =>
+      tOrden(pi.indexOf(i))
+    )
+  }
+
+  def costoRiegoTablon(i: Int, f: Finca, pi: ProgRiego): Int = {
+    val tiempoInicio = tIR(f, pi)(i)
+    val tiempoFinal  = tiempoInicio + treg(f, i)
+
+    if (tsup(f, i) - treg(f, i) >= tiempoInicio) {
+      tsup(f, i) - tiempoFinal
+    } else {
+      prio(f, i) * (tiempoFinal - tsup(f, i))
+    }
+  }
+
+  def costoRiegoFinca(f: Finca, pi: ProgRiego): Int = {
+    (0 until f.length).map(i => costoRiegoTablon(i, f, pi)).sum
+  }
+
+  def costoMovilidad(f: Finca, pi: ProgRiego, d: Distancia): Int = {
+    (0 until pi.length - 1).map(j => d(pi(j))(pi(j + 1))).sum
+  }
+
+}
