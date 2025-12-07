5517de2945d51e0cc708ca75d762641eb4c2860f
Jann Carlo Martinez Cardona <jann.martinez@correounivalle.edu.co>
2025-12-07 12:09:01 -0500

    Se realiza paralelo y benchmarking

--- app/src/test/scala/taller/RiegoOptimoTest.scala
+++ app/src/test/scala/taller/RiegoOptimoTest.scala
@@ -255,5 +255,177 @@ class RiegoOptimoTest extends AnyFunSuite {
     assert(costo == min)
     assert(pi.sorted == Vector(0,1,2))
   }
+  // ==========================
+  // TESTS costoRiegoFincaPar
+  // ==========================
+
+  test("costoRiegoFincaPar: finca de un solo tablón coincide con versión secuencial") {
+    val f = Vector((10,3,1))
+    val pi = Vector(0)
+    assert(RiegoOptimo.costoRiegoFincaPar(f,pi) == RiegoOptimo.costoRiegoFinca(f,pi))
+  }
+
+  test("costoRiegoFincaPar: dos tablones suma paralela igual a secuencial") {
+    val f = Vector((8,2,1), (7,3,2))
+    val pi = Vector(0,1)
+    assert(RiegoOptimo.costoRiegoFincaPar(f,pi) == RiegoOptimo.costoRiegoFinca(f,pi))
+  }
+
+  test("costoRiegoFincaPar: orden alterado mantiene igualdad con versión secuencial") {
+    val f = Vector((10,3,1), (7,2,2))
+    val pi = Vector(1,0)
+    assert(RiegoOptimo.costoRiegoFincaPar(f,pi) == RiegoOptimo.costoRiegoFinca(f,pi))
+  }
+
+  test("costoRiegoFincaPar: varios tablones sin penalización igual al secuencial") {
+    val f = Vector((10,3,1), (12,4,1), (8,2,1))
+    val pi = Vector(0,1,2)
+    assert(RiegoOptimo.costoRiegoFincaPar(f,pi) == RiegoOptimo.costoRiegoFinca(f,pi))
+  }
+
+  test("costoRiegoFincaPar: finca con penalizaciones igual al secuencial") {
+    val f = Vector((5,4,3), (6,5,2))
+    val pi = Vector(0,1)
+    assert(RiegoOptimo.costoRiegoFincaPar(f,pi) == RiegoOptimo.costoRiegoFinca(f,pi))
+  }
+  // ==========================
+  // TESTS costoMovilidadPar
+  // ==========================
+
+  test("costoMovilidadPar: movilidad simple coincide con secuencial") {
+    val pi = Vector(0,1)
+    val d = Vector(Vector(0,5), Vector(5,0))
+    assert(RiegoOptimo.costoMovilidadPar(Vector(),pi,d) == RiegoOptimo.costoMovilidad(Vector(),pi,d))
+  }
+
+  test("costoMovilidadPar: tres tablones coincide con secuencial") {
+    val pi = Vector(0,2,1)
+    val d = Vector(
+      Vector(0,4,3),
+      Vector(4,0,6),
+      Vector(3,6,0)
+    )
+    assert(RiegoOptimo.costoMovilidadPar(Vector(),pi,d) == RiegoOptimo.costoMovilidad(Vector(),pi,d))
+  }
+
+  test("costoMovilidadPar: programa invertido igual al secuencial") {
+    val pi = Vector(2,1,0)
+    val d = Vector(
+      Vector(0,2,4),
+      Vector(2,0,3),
+      Vector(4,3,0)
+    )
+    assert(RiegoOptimo.costoMovilidadPar(Vector(),pi,d) == RiegoOptimo.costoMovilidad(Vector(),pi,d))
+  }
+
+  test("costoMovilidadPar: recorrido largo coincide con secuencial") {
+    val pi = Vector(0,1,2,3)
+    val d = Vector(
+      Vector(0,1,2,3),
+      Vector(1,0,4,5),
+      Vector(2,4,0,6),
+      Vector(3,5,6,0)
+    )
+    assert(RiegoOptimo.costoMovilidadPar(Vector(),pi,d) == RiegoOptimo.costoMovilidad(Vector(),pi,d))
+  }
+
+  test("costoMovilidadPar: todos los movimientos iguales coincide con secuencial") {
+    val pi = Vector(0,1,2)
+    val d = Vector(
+      Vector(0,10,10),
+      Vector(10,0,10),
+      Vector(10,10,0)
+    )
+    assert(RiegoOptimo.costoMovilidadPar(Vector(),pi,d) == RiegoOptimo.costoMovilidad(Vector(),pi,d))
+  }
+  // ==========================================
+  // TESTS generarProgramacionesRiegoPar
+  // ==========================================
+
+  test("generarProgramacionesRiegoPar: finca de 1 tablón igual al secuencial") {
+    val f = Vector((5,2,1))
+    assert(RiegoOptimo.generarProgramacionesRiegoPar(f) == RiegoOptimo.generarProgramacionesRiego(f))
+  }
+
+  test("generarProgramacionesRiegoPar: finca de 2 tablones igual al secuencial") {
+    val f = Vector((5,2,1),(6,3,2))
+    assert(RiegoOptimo.generarProgramacionesRiegoPar(f).toSet == RiegoOptimo.generarProgramacionesRiego(f).toSet)
+  }
+
+  test("generarProgramacionesRiegoPar: finca de 3 tablones produce 6 permutaciones") {
+    val f = Vector((4,2,1),(7,3,2),(5,1,3))
+    assert(RiegoOptimo.generarProgramacionesRiegoPar(f).size == 6)
+  }
+
+  test("generarProgramacionesRiegoPar: todas las permutaciones contienen los índices correctos") {
+    val f = Vector((5,2,1),(7,3,2),(8,4,3))
+    val r = RiegoOptimo.generarProgramacionesRiegoPar(f)
+    assert(r.forall(pi => pi.sorted == Vector(0,1,2)))
+  }
+
+  test("generarProgramacionesRiegoPar: finca de 4 tablones produce 24 permutaciones igual al secuencial") {
+    val f = Vector((5,2,1),(6,3,2),(7,2,3),(9,4,1))
+    assert(RiegoOptimo.generarProgramacionesRiegoPar(f).size == 24)
+    assert(RiegoOptimo.generarProgramacionesRiegoPar(f).toSet ==
+      RiegoOptimo.generarProgramacionesRiego(f).toSet)
+  }
+  // ==========================================
+  // TESTS ProgramacionRiegoOptimoPar
+  // ==========================================
+
+  test("ProgramacionRiegoOptimoPar: finca de 1 tablón igual al secuencial") {
+    val f = Vector((8,3,1))
+    val d = Vector(Vector(0))
+    val (piPar,cPar) = RiegoOptimo.ProgramacionRiegoOptimoPar(f,d)
+    val (piSeq,cSeq) = RiegoOptimo.ProgramacionRiegoOptimo(f,d)
+    assert(piPar == piSeq)
+    assert(cPar == cSeq)
+  }
+
+  test("ProgramacionRiegoOptimoPar: dos tablones coincide con secuencial") {
+    val f = Vector((6,2,1),(8,4,1))
+    val d = Vector(Vector(0,1),Vector(1,0))
+    val rPar = RiegoOptimo.ProgramacionRiegoOptimoPar(f,d)
+    val rSeq = RiegoOptimo.ProgramacionRiegoOptimo(f,d)
+    assert(rPar == rSeq)
+  }
+
+  test("ProgramacionRiegoOptimoPar: tres tablones coincide con mínimo secuencial") {
+    val f = Vector((10,4,1),(7,3,2),(6,2,1))
+    val d = Vector(
+      Vector(0,6,3),
+      Vector(6,0,2),
+      Vector(3,2,0)
+    )
+    val (piPar,cPar) = RiegoOptimo.ProgramacionRiegoOptimoPar(f,d)
+    val costosMin = RiegoOptimo.generarProgramacionesRiego(f)
+      .map(p => (p,RiegoOptimo.costoRiegoFinca(f,p) + RiegoOptimo.costoMovilidad(f,p,d)))
+    assert(cPar == costosMin.map(_._2).min)
+    assert(piPar.sorted == Vector(0,1,2))
+  }
+
+  test("ProgramacionRiegoOptimoPar: evita recorridos costosos igual que secuencial") {
+    val f = Vector((8,3,1),(6,2,1),(9,4,1))
+    val d = Vector(
+      Vector(0,10,10),
+      Vector(10,0,2),
+      Vector(10,2,0)
+    )
+    val rPar = RiegoOptimo.ProgramacionRiegoOptimoPar(f,d)
+    val rSeq = RiegoOptimo.ProgramacionRiegoOptimo(f,d)
+    assert(rPar == rSeq)
+  }
+
+  test("ProgramacionRiegoOptimoPar: debe respetar prioridad alta igual que secuencial") {
+    val f = Vector((6,5,10),(9,4,1),(7,2,1))
+    val d = Vector(
+      Vector(0,2,6),
+      Vector(2,0,4),
+      Vector(6,4,0)
+    )
+    val (piPar,_) = RiegoOptimo.ProgramacionRiegoOptimoPar(f,d)
+    val (piSeq,_) = RiegoOptimo.ProgramacionRiegoOptimo(f,d)
+    assert(piPar.head == piSeq.head)
+  }
 
 }
